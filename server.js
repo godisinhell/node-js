6ab570e0e80de1d2e788055d55d7b5d1e66f3755
jeetbhdr1 <94077917+jeetbhdr1@users.noreply.github.com>
2025-12-02 00:40:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello World!\n So Early.");
+  res.end("Hello World!\n So Early. Wow.");
 });
 
 server.listen(PORT, () => {
